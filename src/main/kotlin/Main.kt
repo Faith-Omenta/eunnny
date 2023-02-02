f8f0d39941911f60d3fe574e31bb22ccd3a6c01c
Faith-Omenta <faithbomenta@gmail.com>
2023-02-02 20:48:21 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+val name = "Nyanchoka"
+    println(name)
+   val age = "23"
+    println(age)
+val weight = "59"
+    println(weight)
+val phoneNumber = "+254767890656"
+    println(phoneNumber)
+val citizen = "Kenyan"
+    println(citizen)
+val isKenyan = "True"
+    print(isKenyan)
+
+}
