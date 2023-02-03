0337d6a50cdf34159b8a499fe84c47ce7b8ed611
Faith-Omenta <faithbomenta@gmail.com>
2023-02-03 08:40:22 +0300

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,62 @@
-fun main() {
-val name = "Nyanchoka"
-    println(name)
-   val age = "23"
-    println(age)
-val weight = "59"
-    println(weight)
-val phoneNumber = "+254767890656"
-    println(phoneNumber)
-val citizen = "Kenyan"
-    println(citizen)
-val isKenyan = "True"
-    print(isKenyan)
+fun main(args: Array<String>) {
+    var nameOne = ("Melvin")
+    println(nameOne)
+    var ageOne = ("26")
+    println(ageOne)
+    var weightOne = ("58kgs")
+    println(weightOne)
+    var phoneNumberOne = (+254789654678)
+    println(phoneNumberOne)
+    var citizenOne = ("Kenyan")
+    println(citizenOne)
+    var isKenyan = true
+    var nameTwo = ("glenah")
+    println(nameTwo)
+    var ageTwo = ("30")
+    println(ageTwo)
+    var weightTwo = ("79kgs")
+    println(weightTwo)
+    var phoneNumberTwo = ("+2547565678876")
+    println(phoneNumberTwo)
+    var citizenTwo = ("Kenyan")
+    println(citizenTwo)
+    var isKenyanTwo = true
+    var nameThree = ("Darline")
+    print(nameThree)
+    var ageThree = ("24")
+    println(ageThree)
+    var weightThree = ("88kgs")
+    println(weightThree)
+    var phoneNumberThree = ("+49987665678")
+    println(phoneNumberThree)
+    var citizenThree = ("German")
+    println(citizenThree)
+    var isKenyanThree = false
+    var namefour = ("phylis")
+    println(namefour)
+    var ageFour = ("30")
+    println(ageFour)
+    var weightFour = ("90kgs")
+    println(weightFour)
+    var phoneNumberFour = ("+25476898654")
+    print(phoneNumberFour)
+    var citizenFour = ("Kenyan")
+    println(citizenFour)
+    var isKenyanFour = true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
